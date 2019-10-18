4f9fddd1c99b89f8f2cb6b5832763a6a4c764e61
unknown <[shrestha_sl@hotmail.com]>
2019-10-18 17:32:38 -0500

    updated navBar

--- src/components/nav/navBar.js
+++ src/components/nav/navBar.js
@@ -1,22 +1,44 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./navBar.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import {NavbarToggler, Navbar, NavbarBrand, NavLink, Collapse, Nav, NavItem, UncontrolledDropdown,DropdownToggle, DropdownMenu,DropdownItem} from 'reactstrap'
+
 
 class NavBar extends Component {
+    state = {
+        isOpen: true
+    }
+
+    toggle = () => {
+        this.setState = {
+            isOpen: !this.state.isOpen
+        }
+    }
+
   render() {
+    
     return (
-        <header>
-        <h1 className="site-title">SMART INVEST<br />
-        </h1>
-        <nav>
-          <ul className="container">
-               <li><Link className="nav-link">Logout</Link></li>
-               <li><Link className="nav-link" exact activeClassName="nav-link-active" to="/login">Login</Link></li>
-          </ul>
-        </nav>
-      </header>
+        <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink data-toggle="modal" data-target="#exampleModal">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/signUp">Signup</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">Logout</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
     );
   }
 }
+
 
 export default NavBar;
