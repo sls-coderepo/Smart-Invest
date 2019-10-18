faa2a2defe479366dcba31aad74b9e29373d3272
unknown <[shrestha_sl@hotmail.com]>
2019-10-18 09:52:07 -0500

    link added to navbar

---
+++ src/components/nav/navBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./navBar.css";
+
+class NavBar extends Component {
+  render() {
+    return (
+        <header>
+        <h1 className="site-title">SMART INVEST<br />
+        </h1>
+        <nav>
+          <ul className="container">
+               <li><Link className="nav-link">Logout</Link></li>
+               <li><Link className="nav-link" exact activeClassName="nav-link-active" to="/login">Login</Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default NavBar;
